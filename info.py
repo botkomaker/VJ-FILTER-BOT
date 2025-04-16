e21a9dba577084fdd11a1687cc14c9c0e3c58919
siam141 <115353753+siam141@users.noreply.github.com>
2025-04-16 11:53:54 +0600

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "7882943518:AAGjB8oiuOMUGtYwiuaT0NXCncvO_9S
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://i.ibb.co/MxZgqYCs/file-1259.jpg')).split()
+PICS = (environ.get('PICS', 'https://i.ibb.co/kgPRfF8c/file-1260.jpg')).split()
 
 
 # Admins & Users
