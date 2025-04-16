6fd8109f54df02f03bde86ed8aeaad9944aa83c4
siam141 <115353753+siam141@users.noreply.github.com>
2025-04-16 22:02:22 +0600

    Update pm_filter.py

--- CloneTechVJ/pm_filter.py
+++ CloneTechVJ/pm_filter.py
@@ -743,7 +743,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
 
     elif query.data == "help":
-        text = "<b>👨‍💻 How To Use Bot :-\n\n🔻 /start - check bot is working or not.\n\n🔻 /stats - check bot files and users.\n\n🔻 /settings - configure clone bot settings ( owner only ).\n\n🔻 /reset - reset all settings to default or none ( owner only ).\n\n🔻 /broadcast - broadcast a message to your bot users ( owner only ).</b>"
+        text = "<b>👨‍💻 How To Use Bot :-\n\n🔻 /start - check bot is working or not.\n\n🔻 /siam - check bot files and users.\n\n🔻 /settings - configure clone bot settings ( owner only ).\n\n🔻 /reset - reset all settings to default or none ( owner only ).\n\n🔻 /broadcast - broadcast a message to your bot users ( owner only ).</b>"
         btn = [[
             InlineKeyboardButton("🔍 ᴀʙᴏᴜᴛ", callback_data="about"),
             InlineKeyboardButton("🏡 ʜᴏᴍᴇ", callback_data="start")
